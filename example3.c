9dfb75613108c4e6ed993c232cff1ced5589512e
Konstantin Lübeck <konstantin.luebeck@student.uni-tuebingen.de>
2017-04-30 14:45:50 +0200

    Implemented fix16_t version for example3.c.

--- example3.c
+++ example3.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 #include "genann.h"
 
+#if DATATYPE == 0 || DATATYPE == 1
 const char *save_name = "example/xor.ann";
+#elif DATATYPE == 2
+const char *save_name = "example/xor.fix16.ann";
+#endif
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     printf("GENANN example 3.\n");
     printf("Load a saved ANN to solve the XOR function.\n");
 
@@ -25,14 +28,30 @@ int main(int argc, char *argv[])
     }
 
 
+    #if DATATYPE == 0 || DATATYPE == 1
     /* Input data for the XOR function. */
-    const double input[4][2] = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+    const datatype input[4][2] = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
 
     /* Run the network and see what it predicts. */
     printf("Output for [%1.f, %1.f] is %1.f.\n", input[0][0], input[0][1], *genann_run(ann, input[0]));
     printf("Output for [%1.f, %1.f] is %1.f.\n", input[1][0], input[1][1], *genann_run(ann, input[1]));
     printf("Output for [%1.f, %1.f] is %1.f.\n", input[2][0], input[2][1], *genann_run(ann, input[2]));
     printf("Output for [%1.f, %1.f] is %1.f.\n", input[3][0], input[3][1], *genann_run(ann, input[3]));
+    #elif DATATYPE == 2
+    /* Input data for the XOR function. */
+    const datatype input[4][2] = {
+        {0x00000000, 0x00000000},
+        {0x00000000, 0x00010000}, 
+        {0x00010000, 0x00000000}, 
+        {0x00010000, 0x00010000}
+    };
+
+    /* Run the network and see what it predicts. */
+    printf("Output for [%1.f, %1.f] is %1.f.\n", fix16_to_float(input[0][0]), fix16_to_float(input[0][1]), fix16_to_float(*genann_run(ann, input[0])));
+    printf("Output for [%1.f, %1.f] is %1.f.\n", fix16_to_float(input[1][0]), fix16_to_float(input[1][1]), fix16_to_float(*genann_run(ann, input[1])));
+    printf("Output for [%1.f, %1.f] is %1.f.\n", fix16_to_float(input[2][0]), fix16_to_float(input[2][1]), fix16_to_float(*genann_run(ann, input[2])));
+    printf("Output for [%1.f, %1.f] is %1.f.\n", fix16_to_float(input[3][0]), fix16_to_float(input[3][1]), fix16_to_float(*genann_run(ann, input[3])));
+    #endif
 
     genann_free(ann);
     return 0;
